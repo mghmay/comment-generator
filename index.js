76f241111d50ef7334b77bedc47bb7e23e2989c2
mghmay <mghmay@gmail.com>
2022-05-11 21:28:38 +0100

    updated functions

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 // get randomiser button from html
 
+const randomiserButton = document.getElementById("btn");
+const commentContainer = document.getElementById("comment");
+
 // object of arrays: nouns(key) : []{value}
 const words = {
-  nouns: ["fridge", "door", "computer", "internet", "wall", "happiness", "fun"],
-  adjectives: ["good", "fantastic", "scam", "terrible"],
+  nouns: [
+    "fridge",
+    "door",
+    "computer",
+    "internet",
+    "wall",
+    "happiness",
+    "fun",
+    "code",
+    "lamp",
+  ],
+  adjectives: [
+    "good",
+    "fantastic",
+    "scam",
+    "terrible",
+    "excellent",
+    "amazing",
+    "rubbish",
+    "bollocks",
+  ],
   phrases: [
     "would highly recommend",
     "waste of money",
@@ -11,31 +33,49 @@ const words = {
     "right bargain",
     "cheap as chips",
     "better deal than maguire",
+    "happy ending",
+    "for the greater good",
   ],
 };
 
 // array of sentences with missing words
 
 const sentences = [
-  "I (phrase) ITcareerChange. It is (adjectives). I had a lot of (nouns)",
-  "I think ITCareerChange is an absolute (phrase). It is (phrase) :) . There was a lot of (nouns) during this course. ",
-  "When I first found ITCareerChange, I was as happy as a (nouns), after reading all the reviews on how (adjectives) the course was, I think it was something (phrase)",
+  "I (phrase) ITcareerChange. It is (adjectives) . I had a lot of (nouns)",
+  "I think ITCareerChange is an absolute (phrase) . It is (adjectives) :) . There was a lot of (nouns) during this course. ",
+  "When I first found ITCareerChange, I was as happy as a (nouns) , after reading all the reviews on how (adjectives) the course was, I think it was something (phrase)",
   "After 2 weeks of starting ITCareerSwitch, I have now finish the course and secured a position at a (nouns) company. I have now got a (adjectives) job. (phrase)",
-  "I think ITCareer is a (adjectives) company, It is not only a (phrase), it is a $(adjectives) idea. What on earth was i (nouns) ing thinking",
+  "I think ITCareer is a (adjectives) company, It is not only a (phrase) , it is a $(adjectives) idea. What on earth was i (nouns) ing thinking",
 ];
 
 const randomElement = (array) => {
-  let randomElementNumber = Math.floor(Math.random() * array.length - 1);
-  return array[randomElementNumber].split(" ");
+  let randomElementNumber = Math.floor(Math.random() * (array.length - 1));
+  return array[randomElementNumber];
 };
+
+const sentenceGenerator = () => {
+  const sentArr = randomElement(sentences).split(" ");
+
+  for (let i = 0; i < sentArr.length; i++) {
+    if (sentArr[i] === "(phrase)") {
+      sentArr[i] = randomElement(words.phrases);
+    }
+    if (sentArr[i] === "(adjectives)") {
+      sentArr[i] = randomElement(words.adjectives);
+    }
+    if (sentArr[i] === "(nouns)") {
+      sentArr[i] = randomElement(words.nouns);
+    }
+  }
+
+  return sentArr.join(" ");
+};
+
 const messageGenerator = () => {
-  return randomElement(sentences);
+  commentContainer.innerHTML = "";
+  const returnValue =
+    sentenceGenerator() + " " + sentenceGenerator() + " " + sentenceGenerator();
+  commentContainer.innerHTML = returnValue;
 };
 
-console.log(randomElement(sentences));
-
-// function return a random element from an array
-
-// function that returns complete sentences - takes sentences with words missing and puts words in the correct places
-
-// combine function which takes a few random completes entences from sentence array, combines them together into a paragraph and returns paragraph on click randomiser button.
+randomiserButton.addEventListener("click", () => messageGenerator());
